abb2b1d39780e0b9f21163fd76de069da61cc074
Luciano <massoni.luciano7@gmail.com>
2023-12-12 23:23:05 -0300

    agregue los assets al pubspec y los llamo desde Image.asset

--- lib/screens/pantallaPelicula.dart
+++ lib/screens/pantallaPelicula.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../models/info_pelicula.dart';
 import '../service/pelicula_service.dart';
 
-
 class PantallaPelicula extends StatelessWidget {
   final int? idPelicula;
 
@@ -11,48 +10,46 @@ class PantallaPelicula extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final PeliculaService peliculaService = PeliculaService();
-    
-     return FutureBuilder<InfoPelicula>(
-      future: peliculaService.obtenerInfoPelicula(idPelicula!),
-      builder: (context, snapshot) {
-        print(snapshot.data);
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Image(image: AssetImage('../assets/loading_poster.gif')) ;
-        } else if (snapshot.hasError) {
-          // Muestra un mensaje de error si hay un problema.
-          return Text('Error al obtener la película: ${snapshot.error}');
-        } else if (!snapshot.hasData) {
-          // Muestra un mensaje si no hay datos disponibles.
-          return const Text('No se encontró información de la película.');
-        } else {
-          // Obtiene la información de la película.
-          final InfoPelicula infoPelicula = snapshot.data!;
-          return Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                icon: const Icon(
-                  Icons.arrow_back_ios_new, 
-                  color: Colors.black
+
+    return FutureBuilder<InfoPelicula>(
+        future: peliculaService.obtenerInfoPelicula(idPelicula!),
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Image.asset("assets/loading_poster.gif");
+          } else if (snapshot.hasError) {
+            // Muestra un mensaje de error si hay un problema.
+            return Text('Error al obtener la película: ${snapshot.error}');
+          } else if (!snapshot.hasData) {
+            // Muestra un mensaje si no hay datos disponibles.
+            return const Text('No se encontró información de la película.');
+          } else {
+            // Obtiene la información de la película.
+            final InfoPelicula infoPelicula = snapshot.data!;
+            return Scaffold(
+              appBar: AppBar(
+                leading: IconButton(
+                  icon:
+                      const Icon(Icons.arrow_back_ios_new, color: Colors.black),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-                onPressed: () => Navigator.of(context).pop(),
-              ), 
-              title: Text(infoPelicula.title ?? "Título de la película"),
-            ),
-            body: ListView(
-              physics: const BouncingScrollPhysics(),
-              children: [
-                Hero(
-                  tag: "peli${infoPelicula.id.toString()}",
-                  child: FadeInImage(
-                    placeholder: AssetImage('../assets/loading_poster.gif'),
-                    image: NetworkImage('https://image.tmdb.org/t/p/w500${infoPelicula.posterPath ?? ""}'),
+                title: Text(infoPelicula.title ?? "Título de la película"),
+              ),
+              body: ListView(
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  Hero(
+                    tag: "peli${infoPelicula.id.toString()}",
+                    child: FadeInImage(
+                      placeholder: AssetImage('../assets/loading_poster.gif'),
+                      image: NetworkImage(
+                          'https://image.tmdb.org/t/p/w500${infoPelicula.posterPath ?? ""}'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          );
-        }
-      }
-    );
+                ],
+              ),
+            );
+          }
+        });
   }
 }
--- lib/widgets/portadaPeliculaWidget.dart
+++ lib/widgets/portadaPeliculaWidget.dart
@@ -2,38 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_final/screens/pantallaPelicula.dart';
 
 class PortadaPeliculaWidget extends StatelessWidget {
-  final  id;
+  final id;
   final String imageUrl;
-  
-   const PortadaPeliculaWidget({required this.id, required this.imageUrl, Key? key}) : super(key: key);
-  
+
+  const PortadaPeliculaWidget(
+      {required this.id, required this.imageUrl, Key? key})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => PantallaPelicula(idPelicula: id),
-          )
-        );
+            context,
+            MaterialPageRoute(
+              builder: (context) => PantallaPelicula(idPelicula: id),
+            ));
       },
       child: Hero(
-        tag: 'peli$id',
-        // child:Image.network(
-        //   imageUrl,
-        //   loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-        //     if (loadingProgress == null) {
-        //       return child;
-        //     } else {
-        //       return  const Image(image: AssetImage('../assets/loading_poster.gif')); 
-        //     }
-        //   },
-        // ),
-        child:FadeInImage(
-          image: NetworkImage(imageUrl),
-          placeholder: AssetImage('../assets/loading_poster.gif'),
-          )
-      ),
+          tag: 'peli$id',
+          // child:Image.network(
+          //   imageUrl,
+          //   loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+          //     if (loadingProgress == null) {
+          //       return child;
+          //     } else {
+          //       return  const Image(image: AssetImage('../assets/loading_poster.gif'));
+          //     }
+          //   },
+          // ),
+          child: FadeInImage(
+              image: NetworkImage(imageUrl),
+              placeholder: Image.asset("assets/loading_poster.gif").image)),
     );
   }
 }
